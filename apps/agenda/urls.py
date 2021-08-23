9bf7799158907103d68e574659da2a4ce99d8307
LeonardoHQ <leoquiroga221@gmail.com>
2021-08-23 07:59:20 -0300

    Reestructuración de dashboard de módulo agenda.

--- apps/agenda/urls.py
+++ apps/agenda/urls.py
@@ -4,7 +4,7 @@ app_name = 'agenda'
 
 urlpatterns = [
     # PAISES
-    path('agenda/list/', ExampleView.as_view(), name='example_view'),
+    path('agenda/list/', ExampleView.as_view(), name='dashboard'),
    # path('paises/add/', PaisesCreateView.as_view(), name='paises_create'),
    # path('paises/update/<int:pk>/', PaisesUpdateView.as_view(), name='paises_update'),
    # path('paises/delete/<int:pk>/', PaisesDeleteView.as_view(), name='paises_delete'),
