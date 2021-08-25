29dc4aade29afd30e3cf214395f01c0d58299090
LeonardoHQ <leoquiroga221@gmail.com>
2021-08-25 14:13:53 -0300

    Commit de emergencia por mal tiempo 2. Aclaraciones en los comentarios.

--- apps/agenda/views/gestionEventos/views.py
+++ apps/agenda/views/gestionEventos/views.py
@@ -34,31 +34,35 @@ class DashboardAgenda(LoginRequiredMixin, ValidatePermissionRequiredMixin, Creat
         return context
 
 
-class UpdateEventosAgenda(LoginRequiredMixin, ValidatePermissionRequiredMixin, UpdateView):
-    model = eventosAgenda
-    form_class = GestionEventosForm
-    success_url = reverse_lazy('agenda:dashboard')
-    permission_required = 'agenda.change_tiposevento'
-    template_name = 'gestionEventos/list.html'
+# La idea es que se trae desde eñ ajax el href + id de elemento a editar. Eso se enlaza con la vista de update que
+# precisamente requiere de un pk. De ahí se accede al formulario cargado (actualizando la página).
 
-    def post(self, request, *args, **kwargs):
-        data = {}
-        try:
-            form = self.get_form()
-            data = form.save()
-        except Exception as e:
-            data['error'] = str(e)
-        return JsonResponse(data)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        # context['entity'] = 'Países'
-        context['update_url'] = self.success_url
-        context['action'] = 'edit'
-        return context
-
-
+# class UpdateEventosAgenda(LoginRequiredMixin, ValidatePermissionRequiredMixin, UpdateView):
+#     model = eventosAgenda
+#     form_class = GestionEventosForm
+#     success_url = reverse_lazy('agenda:dashboard')
+#     permission_required = 'agenda.change_tiposevento'
+#     template_name = 'gestionEventos/list.html'
+#
+#     def post(self, request, *args, **kwargs):
+#         data = {}
+#         try:
+#             form = self.get_form()
+#             data = form.save()
+#         except Exception as e:
+#             data['error'] = str(e)
+#         # No puede devolver un jsonresponse porque los eventos en el calendario no son botones tipo submit.
+#         return JsonResponse(data)
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#
+#         # context['entity'] = 'Países'
+#         context['update_url'] = self.success_url
+#         context['action'] = 'edit'
+#         return context
+#
+#
 # class DeleteEventoAgenda(LoginRequiredMixin, ValidatePermissionRequiredMixin, DeleteView):
 #     model = eventosAgenda
 #     form_class = GestionEventosForm
@@ -67,34 +71,34 @@ class UpdateEventosAgenda(LoginRequiredMixin, ValidatePermissionRequiredMixin, U
 #     def post(self, request, *args, **kwargs):
 #         id = request.POST['pk']
 #         action = request.POST['action']
-
-
-class PaisesUpdateView(LoginRequiredMixin, ValidatePermissionRequiredMixin, UpdateView):
-    model = Paises
-    form_class = PaisesForm
-    template_name = 'paises/create.html'
-    success_url = reverse_lazy('geografico:paises_list')
-    permission_required = 'geografico.change_paises'
-    url_redirect = success_url
-
-    def dispatch(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        return super().dispatch(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        data = {}
-        try:
-            form = self.get_form()
-            data = form.checkAndSave(form, self.url_redirect, request.POST['action'])
-        except Exception as e:
-            data['error'] = str(e)
-        return JsonResponse(data)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Editar País'
-        context['entity'] = 'Países'
-        context['list_url'] = reverse_lazy('geografico:paises_list')
-        context['action'] = 'edit'
-        return context
+#
+#
+# class PaisesUpdateView(LoginRequiredMixin, ValidatePermissionRequiredMixin, UpdateView):
+#     model = Paises
+#     form_class = PaisesForm
+#     template_name = 'paises/create.html'
+#     success_url = reverse_lazy('geografico:paises_list')
+#     permission_required = 'geografico.change_paises'
+#     url_redirect = success_url
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         self.object = self.get_object()
+#         return super().dispatch(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         data = {}
+#         try:
+#             form = self.get_form()
+#             data = form.checkAndSave(form, self.url_redirect, request.POST['action'])
+#         except Exception as e:
+#             data['error'] = str(e)
+#         return JsonResponse(data)
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = 'Editar País'
+#         context['entity'] = 'Países'
+#         context['list_url'] = reverse_lazy('geografico:paises_list')
+#         context['action'] = 'edit'
+#         return context
 
