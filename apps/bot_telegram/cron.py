7641281482db540b9d51bdd5ae5d535169cda2ec
LeonardoHQ <leoquiroga221@gmail.com>
2021-10-29 20:21:49 -0300

    Proceso de creación de cron. A la espera de merge

--- apps/bot_telegram/cron.py
+++ apps/bot_telegram/cron.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django_cron import CronJobBase, Schedule
 from apps.trabajos.models import Trabajos
 from apps.bot_telegram.models import seguimientoTrabajos
@@ -8,7 +10,13 @@ class rastreoTrabajos(CronJobBase):
 	RUN_EVERY_MINS = 1440
 
 	estados = EstadoParametros.objects.get().last()
-	print(estados.estadoFinalizado)
-	trabajos = Trabajos.objects.exclude(estadoTrabajo=[estados.estadoFinalizado,
-	                                                   estados.estadoEntregado,
-	                                                   estados.estadoCancelado])
+	estados_excluidos = [estados.estadoFinalizado.id,
+	                     estados.estadoEntregado.id,
+	                     estados.estadoCancelado.id]
+	trabajos = Trabajos.objects.exclude(estadoTrabajo__in=estados_excluidos)
+
+	for t in trabajos:
+		cant_dias_en_proceso = datetime.date.today() - t.fechaEntrada
+		if cant_dias_en_proceso > t.prioridad.plazoPrioridad:
+			print("Enviar msj")
+			print("Programar job a la espera de en 1h").
