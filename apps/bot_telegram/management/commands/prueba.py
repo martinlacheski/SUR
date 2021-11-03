802f98bd7e5319cc8826cf88a0a3bc897c11d7fd
LeonardoHQ <leoquiroga221@gmail.com>
2021-11-03 13:40:08 -0300

    Correcciones en display de eventos. No deja modificar si el evento fué resuelto y marca en el dashboard si el evento ha sido resuelto.

--- apps/bot_telegram/management/commands/prueba.py
+++ apps/bot_telegram/management/commands/prueba.py
@@ -1,10 +1,19 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count
 from django.utils import timezone
+
+from apps.parametros.models import EstadoParametros
+from apps.trabajos.models import Trabajos
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print(timezone.now())
-        print(timezone.now().today())
-        print(timezone.datetime.now())
-        print(timezone.datetime.today())
+        trabajo = Trabajos.objects.get()
+        estados = EstadoParametros.objects.last()
+        estados_filter = [estados.estadoEspecial.id,
+                          estados.estadoInicial.id,
+                          estados.estadoPlanificado.id]
+        empAEvaluar = Trabajos.objects.exclude(usuarioAsignado=trabajo.usuarioAsignado). \
+            values('usuarioAsignado').annotate(count=Count('id')). \
+            order_by(). \
+            filter(estadoTrabajo__in=estados_filter)
