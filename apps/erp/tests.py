e9316b6a93d19cba7224a28b2b2d98fd028569f5
martinlacheski <martinlacheski@gmail.com>
2021-11-04 20:14:39 -0300

    Trabajando en Auditoria de Ventas

--- apps/erp/tests.py
+++ apps/erp/tests.py
@@ -1,20 +1,4 @@
-result = []
-temperatures = [22,23,21,25,23]
-tamaño = len(temperatures)
-print(tamaño)
-i=0
-for i in range(0, len(temperatures)):
-    print(temperatures[i])
-    try:
-        if i == len(temperatures):
-            dias = temperatures[i-1] - temperatures[i-1]
-        elif temperatures[i] < temperatures[i+1]:
-            dias = temperatures[i+1] - temperatures[i]
-        elif temperatures[i] > temperatures[i+1]:
-            dias = temperatures[i] - temperatures[i+1]
-        else:
-            dias = 0
-        result.append(dias)
-    except:
-        pass
-print(result)
+from django.test import TestCase
+
+# Create your tests here.
+
