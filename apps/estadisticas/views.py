09fd65fa63bc963ded4c0dc498f1f29785e176ba
martinlacheski <martinlacheski@gmail.com>
2021-11-15 14:27:31 -0300

    Trabajando en Estadisticas

--- apps/estadisticas/views.py
+++ apps/estadisticas/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 from django.contrib.auth.mixins import LoginRequiredMixin
