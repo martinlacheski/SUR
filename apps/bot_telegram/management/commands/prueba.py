5d7f6e11d178baea2f9fcd2f63cbfb55aa442075
LeonardoHQ <leoquiroga221@gmail.com>
2021-11-17 11:48:33 -0300

    Progreso en proceso auto de pedido. Faltan UN MILLÓN DE COSAS

--- apps/bot_telegram/management/commands/prueba.py
+++ apps/bot_telegram/management/commands/prueba.py
@@ -6,8 +6,11 @@ from django.conf import settings
 import hashlib
 import random
 
-from apps.erp.models import PedidosSolicitud, DetallePedidoSolicitud,\
-    Productos, PedidoSolicitudProveedor, DetallePedidoSolicitudProveedor
+# Para remover duplicados
+import itertools
+
+from apps.erp.models import PedidosSolicitud, DetallePedidoSolicitud, \
+    Productos, PedidoSolicitudProveedor, DetallePedidoSolicitudProveedor, Pedidos
 
 from apps.parametros.models import EstadoParametros
 from apps.trabajos.models import Trabajos
@@ -19,14 +22,41 @@ from django.urls import reverse
 # Obtención de todos los usuarios que pueden realizar trabajos y cuantos trabajos tiene cada uno.
 class Command(BaseCommand):
     def handle(self, *args, **options):
+
+        # RECORDATORIO: Este proceso se ejecuta únicamente cuando se vence el plazo para respuestas.
+
+        group_det_no_ordenado = []
+        group_det_ordenado = []
         detalles = []
+
+
         respuestas = PedidoSolicitudProveedor.objects.filter(respuesta__isnull=False)
-        for r in respuestas:
-            detalleRespuesta = DetallePedidoSolicitudProveedor.objects.filter(pedidoSolicitudProveedor=r)
-            for d in detalleRespuesta:
-                detalles.append(d)
+        # Solamente analizamos SI HAY respuetas.
+        if len(respuestas) != 0:
 
-       # Falta agarrar los repetidos y ver si el precio es menor.
+            # Se obtienen TODOS los detalles de TODAS las solicitudes que se hayan respondido
+            for r in respuestas:
+                detalleRespuesta = DetallePedidoSolicitudProveedor.objects.filter(pedidoSolicitudProveedor=r)
+                for d in detalleRespuesta:
+                    detalles.append(d)
 
+            # Se agrupan los detalles POR PRODUCTO (lista de listas)
+            for d in detalles:
+                aux_list = []
+                for dd in detalles:
+                    if d.producto == dd.producto:
+                        aux_list.append(dd)
+                group_det_no_ordenado.append(aux_list)
 
+            # Removemos repetidos
+            for elem in group_det_no_ordenado:
+                if elem not in group_det_ordenado:
+                    group_det_ordenado.append(elem)
+
+            # # Análisis de cada grupo
+            # for grupo in group_det_ordenado:
+            #     if len(grupo) == 1: # Solamente un proveedor respondió por un producto en particular.
+            #         prod = grupo[0]
+            #         generarDetalle(prod.costo, prod.cantidad, prod.subtotal, prod.pedidoSolicitudProveedor)
+            #     for g in grupo:
 
