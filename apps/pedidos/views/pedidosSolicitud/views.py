8de3e1d552e568df6666024a45f3e8e15c1b8f24
LeonardoHQ <leoquiroga221@gmail.com>
2021-12-05 15:00:57 -0300

    Envia mail de pedidos. Módulo terminado

--- apps/pedidos/views/pedidosSolicitud/views.py
+++ apps/pedidos/views/pedidosSolicitud/views.py
@@ -2,6 +2,7 @@ from django.contrib.sites.shortcuts import get_current_site
 import json
 import os
 
+from django.core.mail import EmailMultiAlternatives
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.db.models import Q
@@ -44,7 +45,7 @@ class PedidosSolicitudListView(LoginRequiredMixin, ValidatePermissionRequiredMix
                 data = []
                 for i in DetallePedidoSolicitudProveedor.objects.filter(
                         pedidoSolicitudProveedor__pedidoSolicitud=request.POST['id']).exclude(
-                    pedidoSolicitudProveedor__visto__isnull=True):
+                    pedidoSolicitudProveedor__respuesta__isnull=True):
                     proveedor = i.pedidoSolicitudProveedor.proveedor.toJSON()
                     detalle = i.toJSON()
                     data.append({'detalle': detalle,
@@ -467,7 +468,7 @@ class PedidosSolicitudConfirmView(LoginRequiredMixin, ValidatePermissionRequired
                     data = {'id': pedido.id}
                     data['redirect'] = self.url_redirect
 
-                    # Arma link y prepara solicitud para proveedor
+                    # Arma link y prepara solicitud para proveedor. Envía mail.
                     crearSolicitudes(pedido, dominio)
             else:
                 data['error'] = 'No ha ingresado a ninguna opción'
@@ -745,12 +746,17 @@ class PedidosConfirmView(LoginRequiredMixin, ValidatePermissionRequiredMixin, Up
                             pedido.estado = True
                             pedido.usuario = request.user
                             pedido.save()
+                            self.enviar_mail(pedido)
+
+
 
                         # Cambiamos el estado de la solicitud de pedido REALIZADO en True
                         solicitud = PedidosSolicitud.objects.get(id=pedidoSolicitud)
                         solicitud.realizado = True
                         solicitud.save()
                         # ACA se realiza el envio de MAIL
+
+
 
                         # Devolvemos en Data la ID de la Solicitud de Pedido para poder generar la Boleta
                         data = {'id': solicitud.id}
@@ -772,6 +778,30 @@ class PedidosConfirmView(LoginRequiredMixin, ValidatePermissionRequiredMixin, Up
         context['action'] = 'confirm'
         context['pedido'] = self.get_object().id
         return context
+
+    def enviar_mail(self, pedido):
+        detalle = DetallePedido.objects.filter(pedido=pedido)
+        empresa = Empresa.objects.all().last()
+        subject, from_email, to = 'Pedido de productos', settings.EMAIL_HOST_USER, pedido.proveedor.email
+        text_content = 'Mensaje de pedido.'
+        html_content = '<p>Hola, desde ' + empresa.razonSocial + ', les solicitamos los siguientes productos:' \
+                       '<br><br>' + '<table style="margin: 0px auto; border: 1px solid black;">' \
+                       ' <tr align=Center style="border: 1px solid black;"> <th>Producto</th> <th>Marca Ofertada</th>' \
+                       ' <th>Cantidad</th></tr>'
+
+        for d in detalle:
+            marca_ofertada = d.marcaOfertada
+            if not marca_ofertada:
+                marca_ofertada = ''
+            html_content += '<tr align=Center style="border: 1px solid black;"> ' \
+                            '<td style="border: 1px solid black;">' + str(d.producto.descripcion) +\
+                            '</td>' + '<td style="border: 1px solid black;">' + marca_ofertada + '</td>' +\
+                            '<td style="border: 1px solid black;">' + str(d.cantidad) + '</td>' + '</tr>'
+        html_content += '</table> <br><br> Desde ya muchas gracias. Los saludamos atentamente'
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        pass
 
 
 class PedidosDeleteView(LoginRequiredMixin, ValidatePermissionRequiredMixin, UpdateView):
