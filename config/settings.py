5d70d9ab8c51a9eaa143b0a98b954c3ec1b5caf7
martinlacheski <martinlacheski@gmail.com>
2021-12-23 10:25:05 -0300

    Depurado BUGS y generada BASE con datos BASICOS

--- config/settings.py
+++ config/settings.py
@@ -108,20 +108,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        # Reemplazamos por lo siguiente:
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'SUR',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'cli13',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': BASE_DIR / 'db.sqlite3',
+    #     # Reemplazamos por lo siguiente:
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'SUR',
+        'USER': 'postgres',
+        'PASSWORD': 'cli13',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 # Password validation
